1f39710c1a23abf1aa7aa01a05707303dc376c35
inacioads23 <jinacio11@gmail.com>
2024-12-16 12:55:17 -0300

    Criado diretorio PolimorfismoSobrescrita

---
+++ src/dio/oo/asRelacoes/heranca/PolimorfismoSobrescrita/ClasseFilha1.java
@@ -0,0 +1,10 @@
+package dio.oo.asRelacoes.heranca.PolimorfismoSobrescrita;
+
+public class ClasseFilha1 extends ClasseMae {
+	
+	@Override //- Sobreescrita
+	public void metodo1() {
+		System.out.println("Método 1 da Classe Filha 1");
+	}
+
+}
---
+++ src/dio/oo/asRelacoes/heranca/PolimorfismoSobrescrita/ClasseMae.java
@@ -0,0 +1,12 @@
+package dio.oo.asRelacoes.heranca.PolimorfismoSobrescrita;
+
+public class ClasseMae {
+
+	public void metodo1() {
+		System.out.println("Método 1 da Classe Mãe");
+	}
+	
+	public void metodo2() {
+		System.out.println("Método 2 da Classe Mãe");
+	}
+}
