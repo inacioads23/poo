8e25bcdf7b7c788f1471fb95f8b6a317fe0edc36
inacioads23 <jinacio11@gmail.com>
2024-12-17 13:50:21 -0300

    Criado diretorio organizacaoVisibilidade

---
+++ src/dio/oo/organizacaoVisibilidade/pack1/Classe1.java
@@ -0,0 +1,23 @@
+package dio.oo.organizacaoVisibilidade.pack1;
+
+public class Classe1 {
+
+	// Variáveis
+	private String atributo1;
+	protected String atributo2;
+	public String atributo3;
+
+	// Métodos
+	private void metodo1() {
+
+	}
+
+	protected void metodo2() {
+
+	}
+
+	public void metodo3() {
+
+	}
+
+}
---
+++ src/dio/oo/organizacaoVisibilidade/pack1/Classe2.java
@@ -0,0 +1,18 @@
+package dio.oo.organizacaoVisibilidade.pack1;
+
+//Trabalhando com herança [extends]
+public class Classe2 extends Classe1 {
+
+	// Esse método é da própria classe/ local
+	public void metodo() {
+		//Por extender a Classe1, tem acesso a alguns atributos dessa classe. Ver abaixo 
+		
+		//-> VARIÁVEIS <-
+		//atributo2 - Visibilidade protected
+		//atributo3 - Visibilidade public
+		
+		//-> MÉTODOS <-
+		metodo2(); // Visibilidade protected
+		metodo3(); // Visibilidade public
+	}
+}
